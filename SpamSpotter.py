3c513252abedfd10e859f20456419f977edf1f49
Chad Fry <27785270+GlitchKraken@users.noreply.github.com>
2024-05-29 10:38:26 -0500

    Wrote KeyWords
    wrote the keywords module,
    removed and added some sus words
    from the list.

--- SpamSpotter.py
+++ SpamSpotter.py
@@ -2,57 +2,107 @@ import mailparser
 import sys
 import argparse
 
-# handle basic arguments.
-parser = argparse.ArgumentParser(
-prog="SpamSpotter v 0.1",
-description="SpamSpotter is an email Threat-Hunting Tool. Given a list of emails, it will parse each one and give it a potential-risk score and a human-readable risk-breakdown based on the findings of its individual risk-modules.",
-epilog="2024 - By Chad Fry"
-)
-parser.add_argument("-f", help="Choose a single file to examine.", required=False, metavar="Filename")
-parser.add_argument("-d", help="Scan all email files in the current directory (supports .eml and .msg files)", required=False, action="store_true")
-parser.add_argument("-V", help="Use VirusTotal for analysis (Requires API Key) ((I've included one of my own for the projects sake))", required=False, action="store_true")
-# below line of code basically just prints the help option if no args were supplied.
-#Source: https://stackoverflow.com/questions/8259001/python-argparse-command-line-flags-without-arguments
-args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+def main():
+    # Parse and add Arguments....
+    parser = argparse.ArgumentParser(
+    prog="SpamSpotter v 0.1",
+    description="SpamSpotter is an email Threat-Hunting Tool. Given a list of emails, it will parse each one and give it a potential-risk score and a human-readable risk-breakdown based on the findings of its individual risk-modules.",
+    epilog="2024 - By Chad Fry"
+    )
+    parser.add_argument("-f", help="Choose a single file to examine.", required=False, metavar="Filename")
+    parser.add_argument("-d", help="Scan all email files in the current directory (supports .eml and .msg files)", required=False, action="store_true")
+    parser.add_argument("-V", help="Use VirusTotal for analysis (Requires API Key) ((I've included one of my own for the projects sake))", required=False, action="store_true")
+    # below line of code basically just prints the help option if no args were supplied.
+    #Source: https://stackoverflow.com/questions/8259001/python-argparse-command-line-flags-without-arguments
+    args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
 
+    VirusTotal_Api_Key = "4617ea5bb1333b4dfecba3c69d2ec5daf19d53508e74321653b4dd8d36a07741"
+    
+ 
+    
+    # will be used to track emails scanned, as well as their risk-scores and reasons.
+    #email[0] should always be total risk-score then.
+    #email[1...n] should each be a tuple, with risk-score and per-module reason
+    # a dict will be used for keeping track of large numbers of emails. assuming they are all uniquely named.
+    emails = {}
+    
+    email_score_breakdown = []
+    email_score_breakdown.append(0.0)
+    
+    
+    
+    #######################################
+    # Logic for Handling Arguments
+    #######################################
+    if args.V:
+        # might be unnecessary, since we can just check later down the line?
+        print("VirusTotal mode")
+    if args.f != "":
+        
+        print("=======================================")
+        print("Opening " + str(args.f) + " for analysis" )
+        print("=======================================\n")
+        
+        
+        # basic error handling
+        try:
+            # try to create an entry in the emails dict, with the email itself 
+            email = mailparser.parse_from_file(args.f)
+        except Exception as emailReadError:
+            print("[-] Error while trying to open a given email file: " + str(emailReadError))
+            exit(0)
+        
+        
+        
+        # now run the email through each analysis module. maybe pass through a big dictionary where the email name/key has a list of values?
+        riskKeyWords(email)    
+    elif args.f == "":
+        print("Please provide an email to scan, or specify Directory Mode with -d")
+    elif args.d:
+        
+        # this module is responsible for opening every .eml and .msg file in the current directory and
+        # parsing them one-by-one. oh joy!
+        
+        
+        print("Args D was supplied! entering ~DIRECTORY MODE~")
 
-VirusTotal_Api_Key = "4617ea5bb1333b4dfecba3c69d2ec5daf19d53508e74321653b4dd8d36a07741"
 
-# basic testing to make sure we can both check the argument provided, or wether a flag was ever set at all.
-# note that was don't want file mode and directory mode to happen at the same time. 
-# frankly, we should make sure everything works on a single file first.
-
-if args.V:
-    print("VirusTotal mode")
-
-if args.f != "":
-    print("Opening " + str(args.f) + " for analysis" )
-elif args.d:
-    print("Args D was supplied! entering ~DIRECTORY MODE~")
-
-
-
-
-
-# consider writing parsing code where, by default, the program will print out the help-message...
-# but there should be an option to judge a single-file... or every file in the directory with the script.
-
-
-
-
-#mail = mailparser.parse_from_file("filename")
-#print(mail.from_  )
-
+#############################################################################################################
 # below are the functions im kindly referring to as risk-modules. 
 # these will be called on an email (or list of emails) to identify what stands out about the given email.
+#############################################################################################################
 def riskVirusTotal():
     print("VirusTotal")
-def riskKeyWords():
-    print("KeyWords")
+def riskKeyWords(email):
+    # This module is in charge of checking for key-phrases in different parts of the email. 
+    # it will function by checking the email for phrases commonly seen in spam.
     
     
+    # note that before we compare anything to this list, we should remove punctuation and lowercase it.
+    # https://github.com/OOPSpam/spam-words/blob/main/spam-words-EN.txt
+    RiskPhrases = []
+    with open("spam-words-EN.txt", "r")  as spamPhraseFile:
+        RiskPhrases = spamPhraseFile.read().lower().splitlines()
+    # adding in some phrases I could think of that definitely would be annoying and in spam emails...
+    # made sure to include some common spam-emoji's I've seen before too...
+    RiskPhrases += ["f r e e", "urgent notice", "act now", "final warning", "final notice" "XXX", "$$", "$", "$$$", "immediate", "someone tried to log into your account", "suspended", "action required", "singles", "\N{Money Bag}", "\N{warning sign}", "\N{Heavy Dollar Sign}", "\N{Money-Mouth Face}" ]
+    
+    for phrase in RiskPhrases:
+        if phrase in email.body:
+            print("Risky Phrase Found in email!!! : " + phrase)
+    
+    
+    print(RiskPhrases)
+
     
 
+
+
+
+
+if __name__ =="__main__":
+    main()
+    
 # project by Chad Fry, many thanks to the awesome
 # team behind SpamScope! :D
